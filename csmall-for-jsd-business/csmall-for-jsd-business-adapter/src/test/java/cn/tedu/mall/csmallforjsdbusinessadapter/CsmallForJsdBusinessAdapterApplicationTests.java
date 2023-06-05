463b24691652fe03ed68e1cf072609c7f647d9ba
TEDU <83123095@qq.com>
2023-06-05 15:00:48 +0800

    0-JSD2302-teacher-单体架构

---
+++ csmall-for-jsd-business/csmall-for-jsd-business-adapter/src/test/java/cn/tedu/mall/csmallforjsdbusinessadapter/CsmallForJsdBusinessAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.mall.csmallforjsdbusinessadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsmallForJsdBusinessAdapterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ csmall-for-jsd-business/src/test/java/cn/tedu/mall/csmallforjsdbusiness/CsmallForJsdBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.mall.csmallforjsdbusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsmallForJsdBusinessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
